58e6128a7c5554ad586a95021c9203e3587d5342
Josue Jimenez <josuejjimenezd@gmail.com>
2020-02-12 15:21:06 -0400

    db connection complete - models complete

---
+++ Tarea3/Models/DBStudentsContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Tarea3.Models
+{
+    public partial class DBStudentsContext : DbContext
+    {
+        public DBStudentsContext()
+        {
+        }
+
+        public DBStudentsContext(DbContextOptions<DBStudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-646P9691\\SQLEXPRESS01;Database=DBStudents;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.IdStudent);
+
+                entity.Property(e => e.IdStudent).HasColumnName("Id_Student");
+
+                entity.Property(e => e.Career)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(2)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ Tarea3/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarea3.Models
+{
+    public partial class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+        public string Career { get; set; }
+    }
+}
