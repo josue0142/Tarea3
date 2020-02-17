c0a31cc8ce03cd1ef512da5a3edfb6d25f1942a6
Josue Jimenez <josuejjimenezd@gmail.com>
2020-02-16 22:58:21 -0400

    Student List Complete

---
+++ Tarea3/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tarea3.Models;
+
+namespace Tarea3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                using (DBStudentsContext db = new DBStudentsContext())
+                {
+                    var students = db.Student.Include(Student => Student.CareerFkNavigation).ToList();
+
+                    return View(students.ToList());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
--- Tarea3/Startup.cs
+++ Tarea3/Startup.cs
@@ -61,7 +61,7 @@ namespace Tarea3
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Student}/{action=Index}/{id?}");
             });
         }
     }
