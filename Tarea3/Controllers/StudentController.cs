cd212cba78cb6011eb87b4a94a89b4059c38b97e
Josue Jimenez <josuejjimenezd@gmail.com>
2020-02-17 00:28:17 -0400

    Edit Student Complete

--- Tarea3/Controllers/StudentController.cs
+++ Tarea3/Controllers/StudentController.cs
@@ -68,5 +68,59 @@ namespace Tarea3.Controllers
                 throw;
             }
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (DBStudentsContext db = new DBStudentsContext())
+                {
+                    Student student = db.Student.Find(id);
+                    ViewBag.FirsName = student.FirstName;
+                    ViewBag.LastName = student.LastName;
+                    ViewBag.Status = student.Status;
+                    ViewBag.Career = student.CareerFk;
+
+                    Career career = db.Career.Find(student.CareerFk);
+                    ViewBag.CareerName = career.Name;
+
+                    var careers = db.Career.ToList();
+                    ViewBag.careers = careers;
+
+                    return View(student);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            /*Verifica que los datos del modelo recibidos por parametros son validos
+             * (es decir, captura conversiones de tipos de datos, como fechas con formato no válido) 
+             */
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", new { message = "Ah ocurrido un error" });
+            }
+
+            using (DBStudentsContext db = new DBStudentsContext())
+            {
+                Student student2 = db.Student.Find(student.StudentId);
+                student2.FirstName = student.FirstName;
+                student2.LastName = student.LastName;
+                student2.CareerFk = student.CareerFk;
+                student2.Status = student.Status;
+
+                db.SaveChanges(); //Guardamos los cambios en la base de datos
+
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
